cb1d8c77ff5dfdabf5003bf098378ada9f8e21f3
Flechner Romain <rflechner@romcyber.com>
2016-04-12 18:00:57 +0200

    fluent solution

--- src/GildedRose.Console/ImmutabilityHelpers.cs
+++ src/GildedRose.Console/ImmutabilityHelpers.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 
@@ -6,25 +8,211 @@ namespace GildedRose.Console
 {
     public abstract class BusinessRule
     {
-        protected BusinessRule(string itemName)
-        {
-            ItemName = itemName;
-        }
-
-        public string ItemName { get; }
+        internal abstract void Apply(object model);
     }
 
-    public class DependingRule<TModel, TProperty, TTarget> : BusinessRule
+    public abstract class BusinessRule<TModel> : BusinessRule
     {
-        public DependingRule(string itemName, Expression<Func<TModel, TProperty>> dependency) : base(itemName)
+        internal override void Apply(object model)
         {
-            Dependency = dependency;
+            Apply((TModel)model);
         }
 
-        public Expression<Func<TModel, TProperty>> Dependency { get; }
+        public abstract void Apply(TModel model);
 
-        public Func<TModel, TProperty, TTarget> ComputeWith { get; }
+        public void RegisterTo(BusinessRulesEngine engine)
+        {
+            engine.Register(this);
+        }
+    }
 
+    public abstract class BusinessRuleForId<TId, TModel> : BusinessRule<TModel>
+    {
+        readonly Func<TModel, TId> identitiedBy;
+
+        protected BusinessRuleForId(TId[] targets, Func<TModel, TId> identitiedBy)
+        {
+            Targets = targets;
+            this.identitiedBy = identitiedBy;
+        }
+
+        public TId[] Targets { get; }
+
+        protected bool MustExecuteFor(TModel model)
+        {
+            var id = identitiedBy(model);
+            return Targets.Contains(id);
+        }
+    }
+
+    public class CrossInvariantRule<TModel> : BusinessRule<TModel>
+    {
+        private readonly Action<TModel> action;
+        private readonly List<Func<TModel, bool>> exceptions;
+
+        public CrossInvariantRule(Action<TModel> action, List<Func<TModel, bool>> exceptions)
+        {
+            this.action = action;
+            this.exceptions = exceptions;
+        }
+
+        public override void Apply(TModel model)
+        {
+            if (exceptions.Any(exception => exception(model)))
+                return;
+            action(model);
+        }
+    }
+
+    public class ConditionalRule<TId, TModel> : BusinessRuleForId<TId, TModel>
+    {
+        private readonly Func<TModel, bool> when;
+        private readonly Action<TModel> then;
+
+        public ConditionalRule(TId[] targets, 
+            Func<TModel, bool> when, Action<TModel> then, Func<TModel, TId> identitiedBy)
+            : base(targets, identitiedBy)
+        {
+            this.when = when;
+            this.then = then;
+        }
+        
+        public override void Apply(TModel model)
+        {
+            if (!MustExecuteFor(model))
+                return;
+
+            if (when != null)
+            {
+                if (when(model))
+                    then(model);
+            }
+            else
+            {
+                then(model);
+            }
+            
+        }
+    }
+
+    public class BusinessRulesEngine
+    {
+        private readonly Dictionary<Type, List<BusinessRule>> rules = new Dictionary<Type, List<BusinessRule>>();
+
+        public void Register<T>(BusinessRule<T> rule)
+        {
+            var type = typeof (T);
+            if (!rules.ContainsKey(type))
+                rules.Add(type, new List<BusinessRule>());
+            rules[type].Add(rule);
+        }
+
+        public void ApplyRulesOn<T>(IEnumerable<T> models)
+        {
+            foreach (var model in models)
+                ApplyRulesOn(model);
+        }
+
+        public void ApplyRulesOn<T>(T model)
+        {
+            var type = typeof(T);
+            if (!rules.ContainsKey(type))
+                return;
+            var businessRules = rules[type];
+            foreach (var businessRule in businessRules)
+                businessRule.Apply(model);
+        }
+    }
+
+    public class CreateRule
+    {
+        public static RuleBuilder<T> For<T>()
+        {
+            return new RuleBuilder<T>();
+        }
+    }
+
+    public class RuleBuilder<T>
+    {
+        public RuleForIdBuider<TId, T> HavingIds<TId>(params TId[] ids)
+        {
+            return new RuleForIdBuider<TId, T>(ids);
+        }
+
+        public CrossInvariantRuleBuilder<T> All()
+        {
+            return new CrossInvariantRuleBuilder<T>();
+        }
+    }
+
+    public class CrossInvariantRuleBuilder<T>
+    {
+        private Action<T> action;
+        private readonly List<Func<T, bool>> exceptions = new List<Func<T, bool>>();
+
+        public CrossInvariantRuleBuilder<T> DoAllways(Action<T> action)
+        {
+            this.action = action;
+            return this;
+        }
+
+        public CrossInvariantRuleBuilder<T> ExceptedWhen(Func<T, bool> exception)
+        {
+            exceptions.Add(exception);
+            return this;
+        }
+
+        private CrossInvariantRule<T> Build()
+        {
+            return new CrossInvariantRule<T>(action, exceptions);
+        }
+
+        public void RegisterTo(BusinessRulesEngine engine)
+        {
+            Build().RegisterTo(engine);
+        }
+    }
+
+    public class RuleForIdBuider<TId, T>
+    {
+        private readonly TId[] ids;
+
+        public RuleForIdBuider(TId[] ids)
+        {
+            this.ids = ids;
+        }
+        
+        public ConditionalRuleBuilder<TId, T> IdentitiedBy(Func<T, TId> selector)
+        {
+            return new ConditionalRuleBuilder<TId, T>(ids, selector);
+        }
+    }
+
+    public class ConditionalRuleBuilder<TId, T>
+    {
+        private readonly TId[] ids;
+        private Func<T, bool> when;
+        private readonly Func<T, TId> identitiedBy;
+        
+        public ConditionalRuleBuilder(TId[] ids, Func<T, TId> selector)
+        {
+            this.ids = ids;
+            identitiedBy = selector;
+        }
+        
+        public ConditionalRuleBuilder<TId, T> When(Func<T, bool> condition)
+        {
+            when = condition;
+            return this;
+        }
+
+        public ConditionalRule<TId, T> Then(Action<T> then)
+        {
+            if (identitiedBy == null)
+                throw new InvalidOperationException("Please use IdentitiedBy before");
+
+            return new ConditionalRule<TId, T>(ids, when, then, identitiedBy);
+        }
     }
 
     public static class ImmutabilityHelpers
@@ -40,7 +228,7 @@ namespace GildedRose.Console
 
             internal void Assign<TProperty>(Expression<Func<T, TProperty>> getter, TProperty value)
             {
-                Expression expression = getter.Body;
+                var expression = getter.Body;
                 var memberExpression = expression as MemberExpression;
                 if (memberExpression == null)
                     throw new ArgumentException("Please provide a property", "getter");
--- src/GildedRose.Tests/TestAssemblyTests.cs
+++ src/GildedRose.Tests/TestAssemblyTests.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using GildedRose.Console;
 using Xunit;
 
@@ -88,10 +90,121 @@ namespace GildedRose.Tests
         public void When_cloning_item()
         {
             var brie = Inventory.InitialState.AgedBrie;
-            var s2 = brie.With(s => s.Quality, 200).And(s => s.Name, "popo").Clone();
+            var s2 = brie
+                .With(s => s.Quality, 200)
+                .And(s => s.Name, "popo")
+                .Clone();
 
             Assert.NotEqual(brie.Name, "popo");
             Assert.Equal(s2.Name, "popo");
+        }
+
+        [Fact]
+        public void When_ConditionalRule_should_be_executed()
+        {
+            var rule = CreateRule.For<Item>()
+                .HavingIds(Inventory.ItemNames.AgedBrie)
+                .IdentitiedBy(m => m.Name)
+                .When(item => item.SellIn == 2)
+                .Then(item => item.Quality = 1);
+
+            var item1 = new Item
+            {
+                Quality = 4,
+                SellIn = 2,
+                Name = Inventory.ItemNames.AgedBrie
+            };
+
+            rule.Apply(item1);
+
+            Assert.Equal(1, item1.Quality);
+        }
+
+        [Fact]
+        public void When_ConditionalRule_should_not_be_executed()
+        {
+            var rule = CreateRule.For<Item>()
+                .HavingIds(Inventory.ItemNames.AgedBrie)
+                .IdentitiedBy(m => m.Name)
+                .When(item => item.SellIn == 2)
+                .Then(item => item.Quality = 1);
+
+            var item1 = new Item
+            {
+                Quality = 4,
+                SellIn = 7,
+                Name = Inventory.ItemNames.AgedBrie
+            };
+
+            rule.Apply(item1);
+
+            Assert.Equal(4, item1.Quality);
+        }
+
+        [Fact]
+        public void When_using_BusinessRulesEngine_to_describe_domain_rules()
+        {
+            var engine = new BusinessRulesEngine();
+            
+            Func<Item, string> id = item => item.Name;
+
+
+            CreateRule.For<Item>().All()
+                .DoAllways(item => item.SellIn--)
+                .ExceptedWhen(item => item.Name == Inventory.ItemNames.Sulfuras)
+                .RegisterTo(engine);
+
+            CreateRule.For<Item>().All()
+                .DoAllways(item => item.Quality--)
+                .ExceptedWhen(item => new []
+                {
+                    Inventory.ItemNames.AgedBrie,
+                    Inventory.ItemNames.Sulfuras,
+                    Inventory.ItemNames.BackstageTafkal80Etc
+                }.Contains(item.Name))
+                .RegisterTo(engine);
+
+            CreateRule.For<Item>()
+                .HavingIds(Inventory.ItemNames.AgedBrie, Inventory.ItemNames.BackstageTafkal80Etc)
+                .IdentitiedBy(id)
+                .Then(item => item.Quality++)
+                .RegisterTo(engine);
+
+
+            var state1 = Inventory.InitialState;
+            var state2 = Inventory.InitialState;
+            engine.ApplyRulesOn<Item>(state2.Items);
+
+            Assert.Equal(state1.AgedBrie.Quality, 0);
+            Assert.Equal(state2.AgedBrie.Quality, 1);
+            Assert.Equal(state1.AgedBrie.SellIn, 2);
+            Assert.Equal(state2.AgedBrie.SellIn, 1);
+
+            Assert.Equal(state1.Sulfuras.SellIn, 0);
+            Assert.Equal(state2.Sulfuras.SellIn, 0);
+            Assert.Equal(state1.Sulfuras.Quality, 80);
+            Assert.Equal(state2.Sulfuras.Quality, 80);
+
+            Assert.Equal(state1.DexterityVest.SellIn, 10);
+            Assert.Equal(state2.DexterityVest.SellIn, 9);
+            Assert.Equal(state1.DexterityVest.Quality, 20);
+            Assert.Equal(state2.DexterityVest.Quality, 19);
+
+            Assert.Equal(state1.ElixirOfMongoose.SellIn, 5);
+            Assert.Equal(state2.ElixirOfMongoose.SellIn, 4);
+            Assert.Equal(state1.ElixirOfMongoose.Quality, 7);
+            Assert.Equal(state2.ElixirOfMongoose.Quality, 6);
+
+            Assert.Equal(state1.BackstageTafkal80Etc.SellIn, 15);
+            Assert.Equal(state2.BackstageTafkal80Etc.SellIn, 14);
+            Assert.Equal(state1.BackstageTafkal80Etc.Quality, 20);
+            Assert.Equal(state2.BackstageTafkal80Etc.Quality, 21);
+
+            Assert.Equal(state1.ConjuredManaCake.SellIn, 3);
+            Assert.Equal(state2.ConjuredManaCake.SellIn, 2);
+            Assert.Equal(state1.ConjuredManaCake.Quality, 6);
+            Assert.Equal(state2.ConjuredManaCake.Quality, 5);
+
         }
     }
 }
